2a9b5b8c4cdd67bb86cad930e2d6629914d4b031
lsclear <x@x>
2016-04-24 09:40:33 +0900

    Added

--- wireless/tx/README.md
+++ wireless/tx/README.md
@@ -2,3 +2,5 @@
 Used and Edited spectrum_painter
 https://github.com/polygon/spectrum_painter
 
+usage:
+	python run sample.jpg
--- wireless/tx/run.py
+++ wireless/tx/run.py
@@ -3,9 +3,18 @@ import os
 import sys
 
 command = sys.argv
-if len(command) == 2:
-    os.system("python img2iqstream -s 1000000 -l 1 -o bladerf.iq --format bladerf %s" % command[1])
 
-os.system("bladerf-cli -s bladerf.script")
+
+if len(command) == 2:
+    if(command[1] == "test"):
+        os.system("python img2iqstream.py -s 1000000 -l 0.005 -o bladerf.iq --format bladerf sample.jpg")
+    else:
+        os.system("python img2iqstream.py -s 1000000 -l 0.005 -o bladerf.iq --format bladerf %s" % command[1])
+        os.system("bladerf-cli -s bladerf.script")
+else:
+    print ""
+    print "run [image filename]"
+    print ""
+
 
 
